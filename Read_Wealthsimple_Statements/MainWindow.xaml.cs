6de76324080e6f7f56db1509809fb1f39648c77a
fazerlazer2101 <samferrer145@gmail.com>
2024-11-06 13:30:25 -0600

    Side Panel

--- Read_Wealthsimple_Statements/MainWindow.xaml.cs
+++ Read_Wealthsimple_Statements/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using CsvHelper;
 using Microsoft.Win32;
+using System;
 using System.Collections.ObjectModel;
 using System.Globalization;
 using System.IO;
+using System.Numerics;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,6 +27,7 @@ namespace Read_Wealthsimple_Statements
         {
             DataContext = this;
             statements = new ObservableCollection<Statement>();
+            test = new ObservableCollection<ExcelStatement>();
             InitializeComponent();
         }
 
@@ -36,19 +39,19 @@ namespace Read_Wealthsimple_Statements
             public required string description { get; set; }
             public float amount { get; set; }
             public float balance { get; set; }
-            
         }
 
+
+
+        //
         private ObservableCollection<Statement> statements;
 
-
+        //For the list of shares in the listview
         public ObservableCollection<Statement> Entries
         {
             get { return statements; }
             set { statements = value; }
         }
-
-        
 
         private void btnOpenFile_Click(object sender, RoutedEventArgs e)
         {
@@ -57,8 +60,6 @@ namespace Read_Wealthsimple_Statements
             fileDialog.Multiselect = false;
             fileDialog.Title = "Choose a Wealthsimple CSV";
             fileDialog.Filter = "CSV | *.csv";
-
-            
 
             bool? success = fileDialog.ShowDialog();
             if (success == true)
@@ -73,31 +74,132 @@ namespace Read_Wealthsimple_Statements
 
                 var records = csv.GetRecords<Statement>();
 
-                foreach( Statement record in records )
+                foreach(Statement record in records )
                 {
-                    
                     Entries.Add(record);
-
                 }
 
-                float endingBalance = Entries.Last().balance;
-                float dividendsEarned = Entries.Where(x => x.transaction.Equals("DIV")).Sum(x => x.amount);
-                float totalSpent = Entries.Where(x => x.transaction.Equals("BUY")).Sum(x => x.amount);
+                decimal endingBalance = (decimal) Entries.Last().balance;
+                decimal dividendsEarned = (decimal) Entries.Where(x => x.transaction.Equals("DIV")).Sum(x => x.amount);
+                decimal totalSpent = (decimal) Entries.Where(x => x.transaction.Equals("BUY")).Sum(x => x.amount);
 
-                string outputEndingBalance = $"Ending Balance: ${endingBalance}";
-                string outputDividendsEarned = $"Dividends: ${dividendsEarned}";
-                string outputTotalSpent = $"Total Spent: ${totalSpent}";
+                string outputEndingBalance = $"${endingBalance}";
+                string outputDividendsEarned = $"${dividendsEarned}";
+                string outputTotalSpent = $"${Math.Abs(totalSpent)}";
 
-                lblDividendsEarned.Content = outputDividendsEarned;
-                lblEndingBalance.Content = outputEndingBalance;
-                lblTotalSpent.Content = outputTotalSpent;
-
-                
+                lblDividendsEarnedValue.Content = outputDividendsEarned;
+                lblEndingBalanceValue.Content = outputEndingBalance;
+                lblTotalSpentValue.Content = outputTotalSpent;
             }
             else
             {
                 //Nothing picked
             }
         }
+
+        public class ExcelStatement
+        {
+            private string title { get; set; }
+            private string filePath { get; set; }
+
+            //property that is accessible to the outside
+            public string DisplayText => this.toString();
+            public string DisplayFilePath => this.getFilePath();
+
+
+            public ExcelStatement(string title, string filePath)
+            {
+                this.title = title;
+                this.filePath = filePath;
+            }
+
+            public string getFilePath()
+            { 
+                return filePath;
+            }
+
+        
+
+            public string toString()
+            {
+                return this.title;
+            }
+        }
+
+        private ObservableCollection<ExcelStatement> test;
+
+        //Collection of excelStatements
+        public ObservableCollection<ExcelStatement> AvailableStatements
+        {
+            get { return test; }
+            set { test = value; }
+        }
+
+        private void btnChangeDirectory_Click(object sender, RoutedEventArgs e)
+        {
+            //Get the desired filepath
+            OpenFolderDialog folderDialog = new OpenFolderDialog();
+            folderDialog.DefaultDirectory = "Downloads";
+            folderDialog.Multiselect = false;
+            folderDialog.Title = "Choose a Wealthsimple CSV";
+
+            bool? success = folderDialog.ShowDialog();
+            if (success == true)
+            {
+                //Create instance of Statement
+
+
+                string path = folderDialog.FolderName;
+
+                //Get list all excel files
+                string[] files = Directory.GetFiles(path, "*.csv");
+                
+                foreach (string file in files) 
+                {
+                    //Create an instance of the excelstatment and add it to the listbox
+                    
+                    //Get Title
+                    string title = System.IO.Path.GetFileName(file);
+                    AvailableStatements.Add(new ExcelStatement(title, file));
+
+                }
+                
+                
+
+               
+
+
+                //var reader = new StreamReader(path);
+                //var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+                //var records = csv.GetRecords<Statement>();
+
+                //foreach (Statement record in records)
+                //{
+                //    Entries.Add(record);
+                //}
+
+                //decimal endingBalance = (decimal)Entries.Last().balance;
+                //decimal dividendsEarned = (decimal)Entries.Where(x => x.transaction.Equals("DIV")).Sum(x => x.amount);
+                //decimal totalSpent = (decimal)Entries.Where(x => x.transaction.Equals("BUY")).Sum(x => x.amount);
+
+                //string outputEndingBalance = $"${endingBalance}";
+                //string outputDividendsEarned = $"${dividendsEarned}";
+                //string outputTotalSpent = $"${Math.Abs(totalSpent)}";
+
+                //lblDividendsEarnedValue.Content = outputDividendsEarned;
+                //lblEndingBalanceValue.Content = outputEndingBalance;
+                //lblTotalSpentValue.Content = outputTotalSpent;
+            }
+            else
+            {
+                //Nothing picked
+            }
+
+        }
+
+
+
+        
     }
 }
